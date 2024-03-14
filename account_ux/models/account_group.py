6cbf631d688e3943b465d307e5bfe1a173fbbd19
agusmoncada <agus.monc@gmail.com>
2024-03-14 08:42:59 -0300

    Revert "muk"
    This reverts commit 3106881479319087422d77ff3df08b44c08452c6.

--- account_ux/models/account_group.py
+++
@@ -1,9 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from odoo import models, fields
-
-
-class AccountGroup(models.Model):
-    _inherit = 'account.group'
